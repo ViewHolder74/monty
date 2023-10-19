888f3513eed6877bec3c5fff72e253d101ef1ae7
AkosuaTakyiwaa <nhyiradenkyi@gmail.com>
2023-10-18 19:04:31 -0700

    update on stack

---
+++ opcode.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pop: a function that removes element at the top of stack
+ * @temp: temporary pointer to the top node
+ * @next: the next node to be pointed to
+ * Removes element at the top else print error
+ */
+void pop(stack **top)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L<line_number>:cannot pop an empty stack/n");
+		exit(EXIT_FAILURE);
+	}
+	else 
+	{
+		stack *temp = *top;
+		*top = (*top)->next;
+		free(temp);
+	}
+}

52331da9365dccb1aa1b66d428e70d58572e4a5d
Mussa Milanzi <viewholder74@gmail.com>
2023-10-19 14:45:08 -0700

    pop.c

--- pop.c
+++ pop.c
@@ -7,18 +7,18 @@
  * @line_number: line_number
  * Return: no return
 */
-void pop(stack_t **stack, unsigned int line_number)
+void pop(stack_t **top, unsigned int counter)
 {
 	stack_t *temp;
 
-	if (*stack == NULL)
+	if (*top == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 		exit(EXIT_FAILURE);
 	}
-	temp = *stack;
-	*stack = (*stack)->next;
-	if (*stack)
-		(*stack)->prev = NULL;
+	temp = *top;
+	*top = (*top)->next;
+	if (*top)
+		(*top)->prev = NULL;
 	free(temp);
 }

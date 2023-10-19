e85925755565c05186737c8bff53d59e6fc76f77
AkosuaTakyiwaa <nhyiradenkyi@gmail.com>
2023-10-19 14:28:56 -0700

    update

--- pall.c
+++ pall.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * pall - print all values on the stack
- * @stack: pointer to head
+ * pall - a function that print all values on the stack
+ * *stack: pointer to head
  * @line_number: line nu,ber
  * Return: void
  */
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stackIsh **stack, unsigned int line_number)
 {
 	(void)line_number;
 
